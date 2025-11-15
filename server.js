423a322a61c8417e671e0af907992f123c086cad
Jitesh Kumar <jitesh.kumar@snapwiz.com>
2025-11-15 12:10:48 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
